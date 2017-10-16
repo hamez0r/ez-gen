e4dc64db53d794e26cbfd60766fb1b6ea11a8e99
Epure <a0gneu@net.plm.eds.com>
2017-10-16 18:54:49 +0300

    Try 999:

--- app/ez-gen.js
+++ app/ez-gen.js
@@ -27,7 +27,7 @@ EzGen.prototype = {
     for (let projectDir of projectDirs) {
       let projectConfig = JSON.parse(fs.readProjectFile(projectDir))
       console.log(projectConfig)
-      if (projectConfig.platform === 'all' || projectConfig.platform === process.platform)
+      if (!projectConfig.platform || projectConfig.platform === process.platform)
         appProjects.push(new Project(projectConfig, projectDir))
     }
 
